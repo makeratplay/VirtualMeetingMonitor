fcac45f5d5ceb0e9241ca3b99bf0e42ba90cd741
Jes Bak Hansen <jes@copenhagensoftware.com>
2021-03-18 20:11:30 +0100

    Remove unsued fields

--- VirtualMeetingMonitor/IPHeader.cs
+++ VirtualMeetingMonitor/IPHeader.cs
@@ -7,14 +7,7 @@ namespace VirtualMeetingMonitor
     class IPHeader
     {
         //IP Header fields
-        private byte VersionAndHeaderLength;   //Eight bits for version and header length
-        private byte DifferentiatedServices;   //Eight bits for differentiated services (TOS)
-        private ushort TotalLength;            //Sixteen bits for total length of the datagram (header + message)
-        private ushort Identification;         //Sixteen bits for identification
-        private ushort FlagsAndOffset;         //Sixteen bits for flags and fragmentation offset
-        private byte TTL;                      //Eight bits for TTL (Time To Live)
         private readonly byte Protocol;                 //Eight bits for the underlying protocol
-        private short Checksum;                //Sixteen bits containing the checksum of the header (checksum can be negative so taken as short)
         private readonly uint SourceIPAddress;        //Thirty two bits for the source IP Address
         private readonly uint DestinationIPAddress;   //Thirty two bits for destination IP Address
                                                  
@@ -34,28 +27,28 @@ namespace VirtualMeetingMonitor
 
                 //The first eight bits of the IP header contain the version and
                 //header length so we read them
-                VersionAndHeaderLength = binaryReader.ReadByte();
+                _ = binaryReader.ReadByte();
 
                 //The next eight bits contain the Differentiated services
-                DifferentiatedServices = binaryReader.ReadByte();
+                _ = binaryReader.ReadByte();
 
                 //Next sixteen bits hold the total length of the datagram
-                TotalLength = (ushort)IPAddress.NetworkToHostOrder(binaryReader.ReadInt16());
+                _ = (ushort)IPAddress.NetworkToHostOrder(binaryReader.ReadInt16());
 
                 //Next sixteen have the identification bytes
-                Identification = (ushort)IPAddress.NetworkToHostOrder(binaryReader.ReadInt16());
+                _ = (ushort)IPAddress.NetworkToHostOrder(binaryReader.ReadInt16());
 
                 //Next sixteen bits contain the flags and fragmentation offset
-                FlagsAndOffset = (ushort)IPAddress.NetworkToHostOrder(binaryReader.ReadInt16());
+                _ = (ushort)IPAddress.NetworkToHostOrder(binaryReader.ReadInt16());
 
                 //Next eight bits have the TTL value
-                TTL = binaryReader.ReadByte();
+                _ = binaryReader.ReadByte();
 
                 //Next eight represnts the protocol encapsulated in the datagram
                 Protocol = binaryReader.ReadByte();
 
                 //Next sixteen bits contain the checksum of the header
-                Checksum = IPAddress.NetworkToHostOrder(binaryReader.ReadInt16());
+                _ = IPAddress.NetworkToHostOrder(binaryReader.ReadInt16());
 
                 //Next thirty two bits have the source IP address
                 SourceIPAddress = (uint)(binaryReader.ReadInt32());
